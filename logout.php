672c657c3dd446e5108390e193a5340fde563884
almoualid <oualidalmou02@gmail.com>
2023-11-09 16:18:16 +0100

    Logout

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+// Démarrer la session
+session_start();
+
+// Détruire toutes les variables de session
+session_unset();
+
+// Détruire la session
+session_destroy();
+
+// Redirection vers la page de connexion
+header('Location: index.php');
+exit();
+?>
